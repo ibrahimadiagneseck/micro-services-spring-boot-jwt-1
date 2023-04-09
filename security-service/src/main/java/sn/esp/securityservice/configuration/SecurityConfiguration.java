25e0ff6c89148f87bc0edb98e022a435c50f2fdc
Ibrahima Diagne Seck <120041057+ibrahimadiagneseck@users.noreply.github.com>
2023-04-09 20:10:25 +0000

    commit

--- security-service/src/main/java/sn/esp/securityservice/configuration/SecurityConfiguration.java
+++ security-service/src/main/java/sn/esp/securityservice/configuration/SecurityConfiguration.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,13 +18,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
-
-//    private RsakeysConfig rsakeysConfig;
-//    private PasswordEncoder passwordEncoder;
-//
-//    public SecurityConfiguration(PasswordEncoder passwordEncoder) {
-//        this.passwordEncoder = passwordEncoder;
-//    }
 
 
     @Bean
